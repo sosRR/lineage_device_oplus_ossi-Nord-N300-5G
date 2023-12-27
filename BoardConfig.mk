11ae7fbe85513110fc3a1cac45ceb1cf2ba2b705
Russell Reynolds <129880470+sosRR@users.noreply.github.com>
2023-12-26 22:01:55 -0600

    Update BoardConfig.mk
    Set OEM unlock support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-08-01
 
+# Set the OEM unlock support
+BOARD_HAS_OEMLOCK := true
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
